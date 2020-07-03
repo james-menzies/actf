eb6c2267180bfa642bc95f874076f60405b9ce51
redbrickhut <james.r.menzies@gmail.com>
2020-07-03 20:46:18 +1000

    camel match test refactored

--- src/test/java/autocompletetextfield/CamelCaseMatchTest.java
+++ src/test/java/autocompletetextfield/CamelCaseMatchTest.java
@@ -1,21 +1,57 @@
 package autocompletetextfield;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class CamelCaseMatchTest {
 
+    private String youHong = "Cha, Yue-Hong";
+    private String geoff = "O'Reilly, Geoff";
+
     @Test
-    public void testAccuracy(){
-        String object = "Cha, Yue-Hong";
-        assertTrue(CamelCaseMatch.test("c", object));
-        assertFalse(CamelCaseMatch.test("a", object));
-        assertTrue(CamelCaseMatch.test("ch", object));
-        assertFalse(CamelCaseMatch.test("ca", object));
-        assertTrue(CamelCaseMatch.test("cho", object));
-        assertTrue(CamelCaseMatch.test("hoy", object));
-        assertTrue(CamelCaseMatch.test("hongy", object));
-        assertTrue(CamelCaseMatch.test("or", "O'Reilly, Geoff"));
+    public void simpleOneLetterMatch(){
+        assertTrue(CamelCaseMatch.test("c", youHong));
+    }
+
+    @Test
+    public void simpleOneLetterNonMatch() {
+        assertFalse(CamelCaseMatch.test("a", youHong));
+    }
+
+    @Test
+    public void twoLettersFromSameWordMatch() {
+        assertTrue(CamelCaseMatch.test("ch", youHong));
+    }
+
+    @Test
+    public void nonMatchAfterSuccessfulMatches() {
+        assertFalse(CamelCaseMatch.test("chg", youHong));
+    }
+
+    @Test
+    public void midWordTestNewWord() {
+        assertTrue(CamelCaseMatch.test("hoy", youHong));
+    }
+
+    @Test
+    public void hyphenatedCountedAsWholeWord() {
+        assertTrue(CamelCaseMatch.test("yueh", youHong));
+    }
+
+    @Test
+    public void correctStackReevaluateAfterInitialMisMatch() {
+        assertTrue(CamelCaseMatch.test("cho", youHong));
+    }
+
+    @Test
+    public void newWordMatchedAfterWholeWordMatch() {
+        assertTrue(CamelCaseMatch.test("hongy", youHong));
+    }
+
+    @Test
+    public void oneLetterWordMatch() {
+        assertTrue(CamelCaseMatch.test("or", geoff));
     }
 }
