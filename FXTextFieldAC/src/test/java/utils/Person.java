c6d07bf5a47c4c10fa8fa2e91882d51139fd98bc
James Menzies <james.r.menzies@gmail.com>
2020-10-09 16:42:45 +1100

    Files moved, modularity set up

---
+++ FXTextFieldAC/src/test/java/utils/Person.java
@@ -0,0 +1,53 @@
+package utils;
+
+public class Person implements Comparable<Person> {
+
+    private final String firstName;
+    private final String lastName;
+    private final String country;
+    private final String phone;
+    private final String email;
+
+
+    public Person(String firstName, String lastName, String country, String phone, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.country = country;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public String toString() {
+        return String.format( "%S, %s", lastName, firstName);
+    }
+
+    @Override
+    public int compareTo(Person o) {
+
+        int lastComp =
+                lastName.compareTo(o.lastName);
+
+        return lastComp != 0 ? lastComp : firstName.compareTo(o.firstName);
+    }
+}
